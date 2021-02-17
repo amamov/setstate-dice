a165d0aaf0cae8c7a5a29af0b2ba608d6fcd2047
amamov <ysangsuk78@gmail.com>
2021-02-18 03:13:37 +0900

    Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Assume Dice!'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +18,18 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          // flex: 2,
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+            // flex: 2,
+            child: Image.asset('images/dice1.png')),
+      ],
+    );
   }
 }
